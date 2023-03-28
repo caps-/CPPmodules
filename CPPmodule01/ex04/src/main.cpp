f87c89b365d44d26b1579df5cd94b1ea9e15cc52
Sofie Bozzetti <sofiebozzetti@Sofies-MacBook-Pro.local>
2023-03-29 05:54:15 +1030

    cleaned up main.cpp

--- CPPmodule01/ex04/src/main.cpp
+++ CPPmodule01/ex04/src/main.cpp
@@ -6,26 +6,11 @@
 /*   By: pwhittin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 16:27:43 by pwhittin          #+#    #+#             */
-/*   Updated: 2023/03/28 19:29:44 by pwhittin         ###   ########.fr       */
+/*   Updated: 2023/03/29 05:53:58 by pwhittin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_sed.hpp"
-
-
-/*int	main(int argc, char **argv)
-{
-	if (argc == 4)
-	{
-		if (!argv[2][0] || !argv[3][0])
-			std::cerr << RED << ST01 << std::endl;
-		else
-			ft_sed(argv[1], argv[2], argv[3]);
-	}
-	else
-		std::cerr << YEL << ST02 << RST << std::endl;
-	return (0);
-}*/
 
 int	main(int argc, char **argv)
 {
