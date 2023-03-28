f4e5fd93beeb1df067c743817db76ea3c79f00aa
Phil Whittington <pwhittin@F2K028.42adel.org.au>
2023-03-28 19:02:38 +1030

    cleaned up main and make ST05 less enthusisatic

--- CPPmodule01/ex04/inc/ft_sed.hpp
+++ CPPmodule01/ex04/inc/ft_sed.hpp
@@ -6,7 +6,7 @@
 /*   By: pwhittin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 16:31:15 by pwhittin          #+#    #+#             */
-/*   Updated: 2023/03/28 18:54:12 by pwhittin         ###   ########.fr       */
+/*   Updated: 2023/03/28 19:02:14 by pwhittin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,7 @@
 # define ST02 "Usage: ./ft_sed <filename> <string to replace> <replacement string>"
 # define ST03 "Error: can't open the input file. New one written anyway."
 # define ST04 "Done, changes have been written to a new file called "
-# define ST05 ".replace!"
+# define ST05 ".replace."
 
 void	ft_sed(std::string filename, std::string s1, std::string s2);
 
--- CPPmodule01/ex04/src/main.cpp
+++ CPPmodule01/ex04/src/main.cpp
@@ -6,44 +6,22 @@
 /*   By: pwhittin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 16:27:43 by pwhittin          #+#    #+#             */
-/*   Updated: 2023/03/28 18:42:29 by pwhittin         ###   ########.fr       */
+/*   Updated: 2023/03/28 19:01:38 by pwhittin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_sed.hpp"
 
-/*int	main(void)
-{
-	std::string	filename;
-	std::string	s1;
-	std::string	s2;
-
-	std::cout << B_BLK << BAL << RST << std::endl;
-	std::cout << YEL << ST01 << RST;
-	std::cin >> filename;
-	std::cout << YEL << ST02 <<  RST;
-	std::cin >> s1;
-	std::cout << YEL << ST03 << RST;
-	std::cin >> s2;
-
-	ft_sed(filename, s1, s2);
-}*/
 
 int	main(int argc, char **argv)
 {
-	std::string	filename;
-//	std::string s1 = argv[2];
-//	std::string	s2 = argv[3];
-
 	if (argc == 4)
 	{
 		if (!argv[2][0] || !argv[3][0])
 				std::cout << RED << ST01 << std::endl;
 		else
 		{
-			filename = argv[1];
 			ft_sed(argv[1], argv[2], argv[3]);
-//			std::cout << GRN << ST04 << filename << ST05 << RST << std::endl;
 		}
 	}
 	else
