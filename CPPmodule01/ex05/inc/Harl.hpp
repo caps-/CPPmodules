37182a2429f542bc4d9e5c647a0a88238f6f644d
Sofie Bozzetti <sofiebozzetti@Sofies-MacBook-Pro.local>
2023-03-29 06:58:48 +1030

    here we go with ex05

---
+++ CPPmodule01/ex05/inc/Harl.hpp
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pwhittin <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/29 06:03:19 by pwhittin          #+#    #+#             */
+/*   Updated: 2023/03/29 06:58:11 by pwhittin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
+#include <string>
+#include <iostream>
+
+/* Colours for the output. */
+# define RST   "\033[0m" //reset colour
+# define BLK   "\033[30mdd"
+# define RED   "\033[31m"
+# define GRN   "\033[32m"
+# define YEL   "\033[33m"
+# define BLU   "\033[34m"
+# define MAG   "\033[35m"
+# define CYN   "\033[36m"
+# define WHT   "\033[37m"
+# define B_BLK "\033[1m\033[30m" //bold colours
+# define b_RED "\033[1m\033[31m"
+# define b_GRN "\033[1m\033[32m"
+# define b_YEL "\033[1m\033[33m"
+# define b_BLU "\033[1m\033[34m"
+# define b_MAG "\033[1m\033[35m"
+# define b_CYN "\033[1m\033[36m"
+# define b_WHT "\033[1m\033[37m"
+
+/* Big ass line */
+# define BAL	"-----------------------------------------------------------"
+
+class Harl
+{
+	private:
+		void	debug(void);
+		void	info(void);
+		void	warning(void);
+		void	error(void);
+
+	public:
+		Harl();
+		~Harl();
+		void	complain(std::string level);
+}
+
+
+
+#endif
