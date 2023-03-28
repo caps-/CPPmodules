39db2c32b9de35627bbb8baa9c8894542b7eca96
Phil Whittington <pwhittin@F2K028.42adel.org.au>
2023-03-28 16:58:36 +1030

    added header and main

--- CPPmodule01/ex04/inc/ft_sed.hpp
+++ CPPmodule01/ex04/inc/ft_sed.hpp
@@ -6,7 +6,7 @@
 /*   By: pwhittin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 16:31:15 by pwhittin          #+#    #+#             */
-/*   Updated: 2023/03/28 16:40:53 by pwhittin         ###   ########.fr       */
+/*   Updated: 2023/03/28 16:56:04 by pwhittin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,34 @@
 # define FT_SED_HPP
 
 # include <iostream>
+# include <string>
 # include <fstream>
+
+/* Colours for the output. */
+# define RST   "\033[0m" //reset colour
+# define BLK   "\033[30m"
+# define RED   "\033[31m"
+# define GRN   "\033[32m"
+# define YEL   "\033[33m"
+# define BLU   "\033[34m"
+# define MAG   "\033[35m"
+# define CYN   "\033[36m"
+# define WHT   "\033[37m"
+# define B_BLK "\033[1m\033[30m" //bold colours
+# define b_RED "\033[1m\033[31m"
+# define b_GRN "\033[1m\033[32m"
+# define b_YEL "\033[1m\033[33m"
+# define b_BLU "\033[1m\033[34m"
+# define b_MAG "\033[1m\033[35m"
+# define b_CYN "\033[1m\033[36m"
+# define b_WHT "\033[1m\033[37m"
+
+/* Big ass line */
+# define BAL	"-----------------------------------------------------------"
+
+/* Messages to print */
+# define ST01 "Enter the filename: "
+# define ST02 "Enter the string you want replaced : "
+# define ST03 "Enter the string to replace it with: "
 
 #endif
--- CPPmodule01/ex04/src/main.cpp
+++ CPPmodule01/ex04/src/main.cpp
@@ -6,14 +6,22 @@
 /*   By: pwhittin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 16:27:43 by pwhittin          #+#    #+#             */
-/*   Updated: 2023/03/28 16:41:30 by pwhittin         ###   ########.fr       */
+/*   Updated: 2023/03/28 16:58:12 by pwhittin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_sed.hpp"
 
-int	main(int argc, char **argv)
+int	main(void)
 {
-	std::string	filename;
-	std::string	
+	std::string	filename, s1, s2;
+
+	std::cout << YEL <<  ST01 << RST;
+	std::cin >> filename;
+	std::cout << yel << st02 << RST;
+	std::cin << s1;
+	std::cout << yel << st03 << RST;
+	std::cin << s2;
+
+	ft_sed(filename, s1, s2);
 }
