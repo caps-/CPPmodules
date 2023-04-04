d48e017a8bbab895b557a93a969c72b3e1bdb665
Phil Whittington <pwhittin@F2K028.42adel.org.au>
2023-04-04 16:46:40 +0930

    cleaned up number init in main

--- CPPmodule01/ex01/src/main.cpp
+++ CPPmodule01/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pwhittin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 18:45:13 by pwhittin          #+#    #+#             */
-/*   Updated: 2023/03/21 20:23:15 by pwhittin         ###   ########.fr       */
+/*   Updated: 2023/04/04 16:45:10 by pwhittin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int	main(void)
 {
 	Zombie		*zomb;
-	int			number = 0;
+	int			number;
 	std::string	name;
 
 	system("clear");
@@ -25,7 +25,6 @@ int	main(void)
 	std::cout << RST << "Number of zombies: " << MAG;
 	std::cin >> number;
 	std::cout << B_BLK << BAL << RST << std::endl;
-	zomb = zombieHorde(number, name);
+	zomb = zombieHorde(number, name); 
 	delete[] zomb;
-//	free(zomb);
 }
