031d6808b180072ce49adb2d3f7da488e160ad08
Phil Whittington <pwhittin@F2K028.42adel.org.au>
2023-04-04 17:59:12 +0930

    cleaned up colours in header

--- CPPmodule01/ex05/inc/Harl.hpp
+++ CPPmodule01/ex05/inc/Harl.hpp
@@ -6,7 +6,7 @@
 /*   By: pwhittin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 06:03:19 by pwhittin          #+#    #+#             */
-/*   Updated: 2023/04/04 16:11:11 by pwhittin         ###   ########.fr       */
+/*   Updated: 2023/04/04 17:58:57 by pwhittin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,9 @@
 
 /* Colours for the output. */
 # define RST   "\033[0m" //reset colour
-# define BLK   "\033[30mdd"
-# define RED   "\033[31m"
-# define GRN   "\033[32m"
 # define YEL   "\033[33m"
-# define BLU   "\033[34m"
 # define MAG   "\033[35m"
-# define CYN   "\033[36m"
-# define WHT   "\033[37m"
 # define B_BLK "\033[1m\033[30m" //bold colours
-# define b_RED "\033[1m\033[31m"
-# define b_GRN "\033[1m\033[32m"
-# define b_YEL "\033[1m\033[33m"
-# define b_BLU "\033[1m\033[34m"
-# define b_MAG "\033[1m\033[35m"
-# define b_CYN "\033[1m\033[36m"
-# define b_WHT "\033[1m\033[37m"
 
 # define MSG_D "Something about extra bacon and burgers idk"
 # define MSG_I "More bacon blah blah"
