15f00b040b9ed671587f0be88da830d07d80186a
Phil Whittington <pwhittin@F2K028.42adel.org.au>
2023-04-04 17:50:05 +0930

    cleaned up colours in header

--- CPPmodule01/ex00/inc/Zombie.hpp
+++ CPPmodule01/ex00/inc/Zombie.hpp
@@ -6,7 +6,7 @@
 /*   By: pwhittin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 15:21:18 by pwhittin          #+#    #+#             */
-/*   Updated: 2023/03/21 17:10:01 by pwhittin         ###   ########.fr       */
+/*   Updated: 2023/04/04 17:49:46 by pwhittin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,10 @@
 /* Colours for the output. Not going to use them all but I've put them here 
  * any way just in case. */
 # define RST   "\033[0m" //reset colour
-# define BLK   "\033[30m"
 # define RED   "\033[31m"
 # define GRN   "\033[32m"
 # define YEL   "\033[33m"
-# define BLU   "\033[34m"
-# define MAG   "\033[35m"
-# define CYN   "\033[36m"
 # define WHT   "\033[37m"
-# define B_BLK "\033[1m\033[30m" //bold colours
-# define b_RED "\033[1m\033[31m"
-# define b_GRN "\033[1m\033[32m"
-# define b_YEL "\033[1m\033[33m"
-# define b_BLU "\033[1m\033[34m"
-# define b_MAG "\033[1m\033[35m"
-# define b_CYN "\033[1m\033[36m"
-# define b_WHT "\033[1m\033[37m"
 
 # define MSG01 "BraiiiiiiinnnzzzZ..."
 # define MSG02 "*dying noises*"
