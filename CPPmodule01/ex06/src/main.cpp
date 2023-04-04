958cf514333d02e388786eeb0586172dd4e69a98
Phil Whittington <pwhittin@F2K028.42adel.org.au>
2023-04-04 16:21:57 +0930

    Cleaned up makefile and main

--- CPPmodule01/ex06/src/main.cpp
+++ CPPmodule01/ex06/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pwhittin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 06:16:54 by pwhittin          #+#    #+#             */
-/*   Updated: 2023/04/04 16:08:16 by pwhittin         ###   ########.fr       */
+/*   Updated: 2023/04/04 16:16:47 by pwhittin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@ int	main(int argc, char **argv)
 {
 	Harl		harl;
 
-	if (argc != 2)
+	if (argc != 2) //print usage details, msg macros are in the header
 	{
 		std::cout << B_BLK << BAL << std::endl;
-		std::cout << MAG << MSG_USE  << RST << std::endl 
-			<< CYN << MSG_LVL << RST << std::endl;
+		std::cout << MAG << MSG_USE  << RST << std::endl;
+		std::cout << CYN << MSG_LVL << RST << std::endl;
 		return (0);
 	}
 
-	harl.complain(argv[1]);
+	harl.complain(argv[1]); //send whatever level to the complain function
 	return (0);
 }
