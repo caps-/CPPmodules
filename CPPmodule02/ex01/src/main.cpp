ac829897c6090de22baadad0710e81f2dd3cb308
Sofie Bozzetti <sofiebozzetti@Sofies-MacBook-Pro.local>
2023-04-10 17:40:45 +0930

    changed main.cpp to the one we need to use

--- CPPmodule02/ex01/src/main.cpp
+++ CPPmodule02/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pwhittin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 18:39:24 by pwhittin          #+#    #+#             */
-/*   Updated: 2023/04/10 16:43:33 by pwhittin         ###   ########.fr       */
+/*   Updated: 2023/04/10 17:40:13 by pwhittin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,18 @@
 int main(void)
 {
 	Fixed a;
-	Fixed b(a);
-	Fixed c;
-	
-	c = b;
-	
-	std::cout << a.getRawBits() << std::endl;
-	std::cout << b.getRawBits() << std::endl;
-	std::cout << c.getRawBits() << std::endl;
+	Fixed const b( 10 );
+	Fixed const c( 42.42f );
+	Fixed const d( b );
+	a = Fixed( 1234.4321f );
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
 	
 	return 0;
 }
