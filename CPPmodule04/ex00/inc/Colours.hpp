e111d7e6a98b74bf3d3ea1e59eee84cb1bd9cd3c
Phil Whittington <pwhittin@F2K028.42adel.org.au>
2023-04-26 18:57:01 +0930

    here we go

---
+++ CPPmodule04/ex00/inc/Colours.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Colours.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pwhittin <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/26 18:55:12 by pwhittin          #+#    #+#             */
+/*   Updated: 2023/04/26 18:55:18 by pwhittin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COLOURS.HPP
+# define COLOURS_HPP
+
+//regular
+# define BLK   "\033[30m"
+# define RED   "\033[31m"
+# define GRN   "\033[32m"
+# define YEL   "\033[33m"
+# define BLU   "\033[34m"
+# define MAG   "\033[35m"
+# define CYN   "\033[36m"
+# define WHT   "\033[37m"
+
+//bold
+# define B_BLK "\033[1m\033[30m"
+# define B_RED "\033[1m\033[31m"
+# define B_GRN "\033[1m\033[32m"
+# define B_YEL "\033[1m\033[33m"
+# define B_BLU "\033[1m\033[34m"
+# define B_MAG "\033[1m\033[35m"
+# define B_CYN "\033[1m\033[36m"
+# define B_WHT "\033[1m\033[37m"
+
+//reset colours
+# define RST   "\033[0m"
+
+#endif
